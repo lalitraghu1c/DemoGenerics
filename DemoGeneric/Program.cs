1ef47449837f334c21e0cd7db7829edc2f9081d0
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-15 14:45:58 +0530

    [Lalit] Add: Folder Structure Created

---
+++ DemoGeneric/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace DemoGeneric
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArr = { 1, 2, 3, 4, 5 };
+            double[] doubleArr = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            char[] charArr = { 'a', 'b', 'c', 'd', 'e' };
+            GenericOperation genericOperation = new GenericOperation();
+            genericOperation.ToPrintArray(intArr);
+            genericOperation.ToPrintArray(doubleArr);
+            genericOperation.ToPrintArray(charArr);
+        }
+    }
+}
